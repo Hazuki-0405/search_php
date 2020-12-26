c4c74cd3eeb433d2d13d8653059637cb569358e3
EC2 Default User <ec2-user@ip-172-31-3-222.ap-northeast-1.compute.internal>
2020-12-26 02:21:02 +0000

    検索プログラム完成

--- index.php
+++ index.php
@@ -9,30 +9,36 @@
     $search_name_mei =  htmlspecialchars($search_name_mei, ENT_QUOTES, 'UTF-8');
     $search_age = $_GET['age'];
     $search_age =  htmlspecialchars($search_age, ENT_QUOTES, 'UTF-8');
-    $search_datetime = $_GET['datetime'];
-    $search_datetime =  htmlspecialchars($search_datetime, ENT_QUOTES, 'UTF-8');
     
-    $sql = "SELECT * FROM table where 1";
-    $search_data = [];
+    $sql = "SELECT * FROM customer where 1 ";
+    // 検索条件が入っているか
     if($search_id){
-        $sql.= "and id=:id";
+        $sql.= "and id=:id ";
     }
     if($search_name_sei){
-        $sql.= "and name_sei=:name_sei";
+        $sql.= "and name_sei=:name_sei ";
     }
     if($search_name_mei){
-        $sql.= "and name_mei=:name_mei";
+        $sql.= "and name_mei=:name_mei ";
     }
     if($search_age){
-        $sql.= "and age=:age";
-    }
-    if($search_datetime){
-        $sql.= "and datetime=:datetime";
+        $sql.= "and age=:age ";
     }
     $statement = $database->prepare($sql);
-    $statement->bindParam(':id',$search_id);
-    var_dump($statement);
-    // $statement->bindParam(':name_sei',$search_name_sei);
+    
+    // 入ってたら値をバインドする
+    if($search_id){
+        $statement->bindParam(':id', $search_id);
+    }
+    if($search_name_sei){
+        $statement->bindParam(':name_sei', $search_name_sei);
+    }
+    if($search_name_mei){
+        $statement->bindParam(':name_mei', $search_name_mei);
+    }
+    if($search_age){
+        $statement->bindParam(':age', $search_age);
+    }
     $statement->execute();
     $customers = $statement->fetchAll();
 ?>
@@ -51,7 +57,6 @@
             </p>
             <p>
                 年齢<input type="text" name="age"/>
-                作成日時<input type="text" name="datetime"/>
             </p>
             <input type="submit" value="検索"/>
         </form>
@@ -65,15 +70,16 @@
         <table>
             <tr><th>id</th><th>姓</th><th>名</th><th>年齢</th><th>作成日時</th></tr>
             <?php 
-                if($search_id || $search_name_sei)
-                foreach($customers as $customer){
-                    $id = $customer['id'];
-                    $name_sei = $customer['name_sei'];
-                    $name_mei = $customer['name_mei'];
-                    $age = $customer['age'];
-                    $create_datetime = $customer['create_datetime'];
-                    
-                    echo "<tr><td>$id</td><td>$name_sei</td><td>$name_mei</td><td>$age</td><td>$create_datetime</td></tr>";
+                if($search_id || $search_name_sei || $search_name_mei || $search_age || $search_datetime){
+                    foreach($customers as $customer){
+                        $id = $customer['id'];
+                        $name_sei = $customer['name_sei'];
+                        $name_mei = $customer['name_mei'];
+                        $age = $customer['age'];
+                        $create_datetime = $customer['create_datetime'];
+                        
+                        echo "<tr><td>$id</td><td>$name_sei</td><td>$name_mei</td><td>$age</td><td>$create_datetime</td></tr>";
+                    }
                 }
             ?>
         </table>
